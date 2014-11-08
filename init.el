6f2c9c8e5067373202f6654cbc087893dd4bd326
Chris Charles <chris@ccharles.ca>
2014-11-08 12:34:41 -0500

    Disable horizontal scrollbars

--- init.el
+++ init.el
@@ -27,6 +27,7 @@
 
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'horizontal-scroll-bar-mode) (horizontal-scroll-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
 (setq inhibit-startup-message t)
