5f6f7a812a9e2afa3d532396d76fe1af1da709ae
Chris Charles <chris@ccharles.ca>
2015-09-20 17:02:46 -0400

    Use exec-path-from-shell
    This seems to be required on Elementary, and I don't think it will hurt
    anything under other environments.

--- init.el
+++ init.el
@@ -685,6 +685,10 @@
           (setf deft-use-filename-as-title t))
   :bind ("<f8>" . deft))
 
+(use-package exec-path-from-shell
+  :ensure t
+  :config (exec-path-from-shell-initialize))
+
 (provide 'init)
 
 ;;; init.el ends here
